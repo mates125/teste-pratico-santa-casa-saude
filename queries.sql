cba6cee71cbb70e32b9c263c2330a1e2d7ec7932
Mateus Lopes <mtslopes.dev@gmail.com>
2026-03-31 20:18:37 -0300

    feat: queries para consulta de produtos mais vendidos

--- queries.sql
+++ queries.sql
@@ -1,1 +1,7 @@
-#
+-- Esse arquivo é responsável pela consulta que retorna os produtos mais vendidos, ordenado por quantidade.
+
+-- Produtos mais vendidos com faturamento total
+SELECT p.nome AS produto, SUM(v.quantidade) AS total_vendido, SUM(v.valor_total) AS faturamento_total
+FROM vendas v JOIN produtos p ON v.produto_id = p.id
+GROUP BY p.nome
+ORDER BY total_vendido DESC;
